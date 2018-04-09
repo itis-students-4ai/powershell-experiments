053a7313869c49d5a0f44ad6e71efa655699e81e
AgostinelliGiacomo <32615853+AgostinelliGiacomo@users.noreply.github.com>
2018-04-09 19:54:00 +0200

    Script Agostinelli

---
+++ Agostinelli Hello World.ps1
@@ -0,0 +1,7 @@
+# Hello.ps1
+# scrittura del messaggio
+$args = "Giacomo"
+
+write-host "************"
+write-host "Ciao $args" -foregroundcolor yellow -backgroundcolor Black
+write-host "************"
