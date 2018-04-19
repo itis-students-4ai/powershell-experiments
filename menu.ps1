e9e2692a9a91780c44303690692525f5c2c1b383
GIOVANNI.CECCOLINI <giovanni.ceccolini@merloni.local>
2018-04-19 12:14:31 +0200

    Compiti per casa

---
+++ menu.ps1
@@ -0,0 +1,26 @@
+﻿#menù
+#scelta dei comandi
+
+switch($args[0])
+{
+    "D" {Get-Date}
+    "U" {$env:username}
+    "L" {Get-ChildItem}
+    "C" {Get-Location}
+    "X" {cd C:\Users
+         dir
+         cd z:\}
+    "Y" {}
+    "W" {Get-WmiObject -Class Win32_PingStatus -Filter "Address='127.0.0.1'" -ComputerName .}
+    "Z" {ping www.google.com}
+    default { write-host "D = data odierna"
+              write-host "U = utente collegato"
+              write-host "L = lista file"
+              write-host "C = directory corrente"
+              write-host "X = visualizza utenti del sistema"
+              write-host "Y = visualizza utenti loggati"
+              write-host "W = visualizza configurazione della macchina"
+	          write-host "Z = ping di www.google.com"
+	      
+            } 
+}
