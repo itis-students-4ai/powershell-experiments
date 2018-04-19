c429a679f16f9849b849c54b3029dd507595cb3e
tex <fra99ilmito@gmail.com>
2018-04-19 22:04:22 +0200

    Aggiunto programma ma ci sono problemi con comandi

---
+++ Menu.ps1
@@ -0,0 +1,22 @@
+#
+#programma Francesco Migliaccio Tessitore	
+switch($args[0])
+{
+	"D" {Get-Date}	
+	"U" {$env:username}
+	"L" {Get-ChildItem}
+	"C" {Get-Location}
+	"W" {Get-NetIpConfiguration | Format_List IPv4Address}
+	"X" {dir -path C:\Users}
+	"Y" { Select-Object UserName}
+	"Z" {Test-Connection 8.8.8.8}
+
+	default {Write-Host "D = Data di Oggi"
+		 Write-Host "U = Utente attualmente collegato"
+		 Write-Host "L = Lista dei file"
+		 Write-Host "C = Directory corrente"
+		 Write-Host "W = Visualizza la configurazione di rete"
+		 Write-Host "X = Visualizza tutti gli utenti del sistema"
+		 Write-Host "Y = Visualizza gli utenti connessi"
+		 Write-Host "Z = Ping a 8.8.8.8"}
+} 
