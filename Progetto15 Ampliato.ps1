9cb8efb78c4865ca0d6abad93f38e93615cd21a7
YAGO LAUTARO.SORO <yago.soro@merloni.local>
2018-04-20 12:11:03 +0200

    Miglioramento del comando relativo alla lettera "W"

--- Progetto15 Ampliato.ps1
+++ Progetto15 Ampliato.ps1
@@ -6,18 +6,20 @@ switch($args[0])
 	"U" {$env:username}
 	"L" {Get-ChildItem}
 	"C" {Get-Location}
-	"W" {Get-NetIpConfiguration | Format_List IPv4Address}
+	"W" {ipConfig | select-string "Indirizzo IPv4"
+		 ipConfig | select-string "Subnet Mask"
+		 ipConfig | select-string "Gateway Predefinito"}
 	"X" {Get-ChildItem -path C:\Users}
 	"Y" {}
 	# Non sono riuscito a trovare un comando che mi permettesse di visualizzare gli utenti attualmente connessi
 	"Z" {Test-Connection www.google.com}
 
 	default {Write-Host "D = Data di Oggi"
-		 Write-Host "U = Utente attualmente collegato"
-		 Write-Host "L = Lista dei file"
-		 Write-Host "C = Directory corrente"
-		 Write-Host "W = Visualizzazione configurazione di rete"
-		 Write-Host "X = Visualizzazione utenti possibili nel sistema"
-		 Write-Host "Y = Visualizzazione utenti connessi"
-		 Write-Host "Z = Ping a www.google.com"}
+		 	 Write-Host "U = Utente attualmente collegato"
+		 	 Write-Host "L = Lista dei file"
+		 	 Write-Host "C = Directory corrente"
+		 	 Write-Host "W = Visualizzazione configurazione di rete"
+		 	 Write-Host "X = Visualizzazione utenti possibili nel sistema"
+		 	 Write-Host "Y = Visualizzazione utenti connessi"
+		 	 Write-Host "Z = Ping a www.google.com"}
 }
