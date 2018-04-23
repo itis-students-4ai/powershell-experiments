b06b8582113be57d5e597080789ca963e2b04662
MrHlbi <35236555+MrHlbi@users.noreply.github.com>
2018-04-23 21:12:35 +0200

    Svolto l'esercizio del menù, Albi Sabahu

---
+++ MenuAlbi.ps1
@@ -0,0 +1,20 @@
+switch($args[0])
+{
+	"D" {Get-Date}
+	"U" {$env:username}
+	"L" {Get-ChildItem}
+	"C" {Get-Location}
+	"W" {Get-NetIpConfiguration | Format_List IPv4Address}
+	"X" {Get-ChildItem -path C:\Users}
+	"Y" {}
+	"Z" {Test-Connection www.google.com}
+
+	default {Write-Host "D = Data di Oggi"
+		 Write-Host "U = Utente attualmente collegato"
+		 Write-Host "L = Lista dei file"
+		 Write-Host "C = Directory corrente"
+		 Write-Host "W = Visualizzazione configurazione di rete"
+		 Write-Host "X = Visualizzazione utenti possibili nel sistema"
+		 Write-Host "Y = Visualizzazione utenti connessi"
+		 Write-Host "Z = Ping a www.google.com"}
+}  
