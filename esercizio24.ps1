0e81ee0c9ae96d280dad0feb5ce7f5435a38380f
ALBI.SABAHU <albi.sabahu@merloni.local>
2018-05-04 12:52:14 +0200

    Esercizi 4 maggio in classe

---
+++ esercizio24.ps1
@@ -0,0 +1,1 @@
+get-command | format-table -property name,definition
---
+++ esercizio27.ps1
@@ -0,0 +1,10 @@
+$valore = $args [0]
+
+if ($valore -gt 10)
+{
+	write-host "maggiore di 10"
+}
+else
+{
+	write-host "minore di 10"
+}
